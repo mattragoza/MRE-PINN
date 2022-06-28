21898d0aab87a40c2b5b379ffa6214411c38a759
mattragoza <mtr22@pitt.edu>
2022-06-27 23:05:36 -0400

    try learning u and laplace u instead of mu

---
+++ mre_pinn/discrete.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+grad = np.gradient
+
+
+def jacobian(u, resolution, i, j):
+    return grad(u[...,i], axis=j) / resolution
+
+
+def hessian(u, resolution, component, i, j):
+    return grad(grad(u[...,component], axis=i), axis=j) / resolution**2
+
+
+def laplacian(u, resolution=1, dim=0):
+    '''
+    Discrete Laplacian operator.
+
+    Args:
+        u: (..., M) output tensor.
+        resolution: Input resolution.
+        dim: Summation start axis.
+    Returns:
+        v: (..., M) Laplacian tensor.
+    '''
+    components = []
+    for i in range(u.shape[-1]):
+        component = 0
+        for j in range(dim, u.ndim - 1):
+            component += hessian(u, resolution, component=i, i=j, j=j)
+        components.append(component)
+    return np.stack(components, axis=-1)
+
+
+def helmholtz_inversion(u, Lu, omega, rho=1000):
+    '''
+    Direct algebraic inversion
+    of the Helmholtz equation.
+    '''
+    return (-rho * (2 * np.pi * omega)**2 * u / Lu).mean(axis=-1)
--- mre_pinn/pde.py
+++ mre_pinn/pde.py
@@ -2,25 +2,42 @@ import numpy as np
 import torch
 import deepxde
 
+hessian = deepxde.grad.hessian
+
 
 def laplacian(u, x, dim=0):
+    '''
+    Continuous Laplacian operator.
+
+    Args:
+        u: (N, M) output tensor.
+        x: (N, K) input tensor.
+        dim: Summation start index.
+    Returns:
+        v: (N, M) Laplacian tensor.
+    '''
     components = []
     for i in range(u.shape[1]):
         component = 0
         for j in range(dim, x.shape[1]):
-            component += deepxde.grad.hessian(u, x, component=i, i=j, j=j)
+            component += hessian(u, x, component=i, i=j, j=j)
         components.append(component)
     return torch.cat(components, dim=1)
 
 
 class WaveEquation(object):
     '''
-    μ∇²u = -ρω²u
+    ∇·σ + ρω²u = 0
     '''
-    def __init__(self, detach, rho=1000, dx=1):
+    def __init__(
+        self, detach, rho=1000, dx=1, homogeneous=False, debug=False
+    ):
         self.detach = detach
         self.rho = rho
-        self.dx  = dx
+        self.dx = dx
+        self.homogeneous = homogeneous
+        self.incompressible = True
+        self.debug = debug
 
     def __call__(self, x, outputs):
         '''
@@ -31,12 +48,28 @@ class WaveEquation(object):
             (N x 3) tensor of PDE residual for each
                 ux,uy,uz displacement component
         '''
-        u, mu = outputs[:,:-1], outputs[:,-1:]
+        if self.debug:
+            u, lu = outputs[:,0:2], outputs[:,2:]
+            laplace_u = laplacian(u, x, dim=1) / self.dx**2
+            return laplace_u.detach() - lu
+
+        u, mu = outputs[:,0:-1], outputs[:,-1:]
         omega = x[:,:1]
-        lu = laplacian(u, x, dim=1) / self.dx**2
-        if self.detach:
-            u, lu = u.detach(), lu.detach()
-        return mu * lu + self.rho * (2*np.pi*omega)**2 * u
+        f = self.rho * (2 * np.pi * omega)**2 * u
+
+        if self.homogeneous:
+
+            # Helmholtz equation
+            laplace_u = laplacian(u, x, dim=1) / self.dx**2
+            div_stress = mu * laplace_u
+        else:
+            # Barnhill 2017
+            div_stress = mu * 0
+        
+        if self.detach: # only backprop through mu
+            u, laplace_u = u.detach(), laplace_u.detach()
+
+        return div_stress + f
 
 
 def lvwe(x, u, mu, lam, rho, omega):
