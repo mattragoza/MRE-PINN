6d5e6a93f3dcf98789c2e8fc035ac71ca1b1ca2b
mattragoza <mtr22@pitt.edu>
2022-07-07 18:39:25 -0400

    refactor PINN model

--- mre_pinn/model.py
+++ mre_pinn/model.py
@@ -1,43 +1,23 @@
 import numpy as np
 import torch
-import deepxde
 
-dtype = deepxde.config.real(torch)
-
-
-def as_complex(t):
-    '''
-    Combine the even and odd indices of a real
-    tensor into the real and imaginary parts of
-    a complex tensor.
-
-    Args:
-        t: (N, 2M) real-valued tensor.
-    Returns:
-        An (N, M) complex-valued tensor.
-    '''
-    N, K = t.shape
-    assert K % 2 == 0
-    return torch.complex(t[:,::2], t[:,1::2])
-
-
-def as_real(t):
-    '''
-    Interleave the real and imaginary parts of a
-    complex tensor into the even and odd indices
-    of a real tensor.
-
-    Args:
-        t: (N, M) complex-valued tensor.
-    Returns:
-        An (N, 2M) real-valued tensor.
-    '''
-    N, M = t.shape
-    return torch.stack([t.real, t.imag], dim=-1).reshape(N, 2*M)
+from .utils import identity, as_iterable
 
 
 class PINN(torch.nn.ModuleList):
-    
+    '''
+    A physics-informed neural network.
+
+    Args:
+        n_input: Number of input units.
+        n_layers: Number of linear layers.
+        n_hidden: Number of hidden units.
+        n_output: Number of output units.
+        activ_fn: Activation function(s).
+        input_fn: Input transformation.
+        output_fn: Output transformation.
+        dense: If True, use dense connections.
+    '''
     def __init__(
         self,
         n_input,
@@ -45,85 +25,86 @@ class PINN(torch.nn.ModuleList):
         n_hidden,
         n_output,
         activ_fn,
-        complex=False,
+        input_fn=None,
+        output_fn=None,
         dense=False,
-        omega0=30
+        dtype=torch.float32
     ):
         super().__init__()
 
-        if complex:
-            n_output *= 2
-            self.output_fn = as_complex
-        else:
-            self.output_fn = lambda x: x
+        activ_fn = as_iterable(activ_fn)
+        n_activ_fns = len(activ_fn)
 
         self.linears = []
         for i in range(n_layers):
-            if i < n_layers - 1:
-                linear = torch.nn.Linear(n_input, n_hidden)
-            else:
-                linear = torch.nn.Linear(n_input, n_output)
+
+            if i < n_layers - 1: # hidden layer
+                linear = []
+                for j in range(n_activ_fns):
+                    linear.append(torch.nn.Linear(n_input, n_hidden, dtype=dtype))
+                    self.add_module(f'linear{i}_{activ_fn[j].__name__}', linear[j])
+
+            else: # output layer
+                linear = torch.nn.Linear(n_input, n_output, dtype=dtype)
+                self.add_module(f'linear{i}', linear)
+
             self.linears.append(linear)
-            self.add_module(f'linear{i}', linear)
+
             if dense:
                 n_input += n_hidden
             else:
                 n_input = n_hidden
 
         self.activ_fn = activ_fn
-        self.omega0 = omega0
+        self.input_fn = input_fn or identity
+        self.output_fn = output_fn or identity
         self.dense = dense
 
-    def forward(self, input):
-        input = (input - self.input_loc) / self.input_scale
-        
-        for i, linear in enumerate(self.linears):
-            omega = self.omega0 if i == 0 else 1
 
-            if i < len(self.linears) - 1:
-                output = self.activ_fn(omega * linear(input))
-            else:
+    def forward(self, input):
+
+        # apply input transformation
+        input = self.input_fn(input)
+
+        # forward pass through hidden layers
+        for i, linear in enumerate(self.linears):
+
+            if i < len(self.linears) - 1: # hidden layer
+                output = 1
+                for j, linear in enumerate(linear):
+                    output *= self.activ_fn[j](linear(input))
+
+                if self.dense: # dense connections
+                    input = torch.cat([input, output], dim=1)
+                else:
+                    input = output
+
+            else: # output layer
                 output = linear(input)
 
-            if self.dense:
-                input = torch.cat([input, output], dim=1)
-            else:
-                input = output
-
-        output = output * self.output_scale + self.output_loc
+        # apply output transformation
         return self.output_fn(output)
 
-    def init_weights(
-        self, c=6, input_loc=0, input_scale=1, output_loc=0, output_scale=1
-    ):
+    def init_weights(self, omega0, c=6):
+        '''
+        SIREN weight initialization.
+        '''
         for i, module in enumerate(self.children()):
             n_input = module.weight.shape[-1]
 
-            if i == 0:
-                w_std = 1 / n_input
+            if i == 0: # first layer
+                w_std = omega0 / n_input
             else:
                 w_std = np.sqrt(c / n_input)
 
             with torch.no_grad():
                 module.weight.uniform_(-w_std, w_std)
 
-        self.input_scale = torch.as_tensor(
-            input_scale, device=module.weight.device
-        ).unsqueeze(0)
-        self.input_loc = torch.as_tensor(
-            input_loc, device=module.bias.device
-        ).unsqueeze(0)
-
-        self.output_scale = torch.as_tensor(
-            output_scale, device=module.weight.device
-        ).unsqueeze(0)
-        self.output_loc = torch.as_tensor(
-            output_loc, device=module.bias.device
-        ).unsqueeze(0)
-
 
 class Parallel(torch.nn.ModuleList):
-    
+    '''
+    A parallel container. Applies the forward pass of each child module
+    to the input and then concatenates their output along the second dim.
+    '''
     def forward(self, input):
-        outputs = [module(input) for module in self]
-        return torch.cat(outputs, dim=1)
+        return torch.cat([module(input) for module in self], dim=1)
