96bc2e1d81f5c8197d995e930341591b1c9048b3
mattragoza <mtr22@pitt.edu>
2022-09-01 15:28:42 -0400

    fix 1D FEM oscillations by using DG mu elems or aligning cells, not nodes

--- mre_pinn/fem.py
+++ mre_pinn/fem.py
@@ -9,69 +9,114 @@ from petsc4py.PETSc import ScalarType as dtype
 from .utils import as_xarray
 
 
+def parse_elem_type(s):
+    family, degree = s.split('-')
+    return family, int(degree)
+
+
+def create_mesh_from_data(data, align_nodes):
+    '''
+    Create a mesh from the provided xarray data.
+
+    Args:
+        data: An xarray with spatial dims.
+        align_nodes: If True, align the mesh nodes
+            to the data points, otherwise align
+            the mesh cells to the data points.
+    Returns:
+        A dolfinx mesh.
+    '''
+    # spatial dimensionality, points, and bounds
+    ndim = data.field.n_spatial_dims
+    x = data.field.spatial_points()
+    x_min = x.min(axis=0)
+    x_max = x.max(axis=0)
+    x_res = x[1] - x[0]
+
+    if align_nodes: # align nodes to data
+        bounds = [x_min, x_max]
+        n_cells = [d - 1 for d in data.field.spatial_shape]
+
+    else: # align cells to data
+       bounds = [x_min - x_res/2, x_max + x_res/2]
+       n_cells = data.field.spatial_shape
+
+    # discretize the domain on a mesh
+    if ndim == 2:
+        mesh = dolfinx.mesh.create_rectangle(
+            comm=MPI.COMM_WORLD,
+            points=bounds,
+            n=n_cells,
+            cell_type=dolfinx.mesh.CellType.triangle,
+            diagonal=dolfinx.mesh.DiagonalType.right_left
+        )
+    elif ndim == 1:
+        mesh = dolfinx.mesh.create_interval(
+            comm=MPI.COMM_WORLD,
+            points=bounds,
+            nx=n_cells[0]
+        )
+    return mesh
+
+
+def create_func_from_data(data):
+
+    ndim = data.field.n_spatial_dims
+    x = data.field.spatial_points()
+    y = data.field.values()
+
+    if ndim > 1:
+        interp = scipy.interpolate.LinearNDInterpolator(x, y)
+    else:
+        interp = scipy.interpolate.interp1d(
+            x[:,0], y[:,0], bounds_error=False, fill_value='extrapolate'
+        )
+    def f(x):
+        return np.ascontiguousarray(interp(x[:ndim].T).T)
+    return f
+
+
 class FEM(object):
 
-    def __init__(self, data, n_mesh=None, extend=0):
-
+    def __init__(
+        self,
+        data,
+        u_elem_type='CG-1',
+        mu_elem_type='DG-0',
+        align_nodes=False
+    ):
         self.data = data
-        ndim = data.field.n_spatial_dims
-        x = data.u.field.spatial_points()
-        u = data.u.field.values()
-        x_res = x[1] - x[0] if extend else 0
-        print(x_res, data.u.field.spatial_shape)
+        self.mesh = create_mesh_from_data(data, align_nodes)
 
-        # discretize the domain on a mesh
-        if ndim == 2:
-            self.mesh = dolfinx.mesh.create_rectangle(
-                comm=MPI.COMM_WORLD,
-                points=[x.min(axis=0) - extend, x.max(axis=0) + extend],
-                n=data.u.field.spatial_shape,
-                cell_type=dolfinx.mesh.CellType.triangle,
-                diagonal=dolfinx.mesh.DiagonalType.right_left
-            )
-        elif ndim == 1:
-            self.mesh = dolfinx.mesh.create_interval(
-                comm=MPI.COMM_WORLD,
-                points=[x.min(axis=0) - extend, x.max(axis=0) + extend],
-                nx=n_mesh or data.u.field.spatial_shape[0]
-            )
+        # determine the FEM element types
+        self.u_elem_type = parse_elem_type(u_elem_type)
+        self.mu_elem_type = parse_elem_type(mu_elem_type)
 
         # define the FEM basis function spaces
+        ndim = data.field.n_spatial_dims
         self.scalar_space = dolfinx.fem.FunctionSpace(
-            self.mesh, ('DG', 1)
+            self.mesh, self.mu_elem_type
         )
         self.vector_space = dolfinx.fem.VectorFunctionSpace(
-            self.mesh, ('CG', 1), dim=ndim
+            self.mesh, self.u_elem_type, dim=ndim
         )
         self.tensor_space = dolfinx.fem.TensorFunctionSpace(
-            self.mesh, ('CG', 1), shape=(ndim, ndim)
+            self.mesh, self.u_elem_type, shape=(ndim, ndim)
         )
 
-        # setup physical problem and function spaces
-        self.rho = 1000
-        self.omega = 2 * np.pi * data.frequency.values.item()
-
-        # function for interpolating wave image into FEM basis
-        if ndim > 1:
-            u_interp = scipy.interpolate.LinearNDInterpolator(x, u)
-        else:
-            u_interp = scipy.interpolate.interp1d(
-                x[:,0], u[:,0], bounds_error=False, fill_value='extrapolate'
-            )
-        self.u_interp = u_interp
+        # wave image-interpolating function
         self.u_func = dolfinx.fem.Function(self.vector_space)
-
-        def f(x):
-            return np.ascontiguousarray(u_interp(x[:ndim].T).T)
-
-        self.u_func.interpolate(f)
+        self.u_func.interpolate(create_func_from_data(data.u))
 
         # trial and test functions
         self.mu_func = ufl.TrialFunction(self.scalar_space)
         self.v_func = ufl.TestFunction(self.vector_space)
-        self.mu_pred_func = None
 
     def solve(self):
+
+        # physical constants
+        rho = 1000
+        omega = 2 * np.pi * self.data.frequency.values.item()
 
         # construct bilinear form
         Auv = self.mu_func * ufl.inner(
@@ -79,7 +124,7 @@ class FEM(object):
         ) * ufl.dx
 
         # construct inner product
-        bv = self.rho * self.omega**2 * ufl.inner(
+        bv = rho * omega**2 * ufl.inner(
             self.u_func, self.v_func
         ) * ufl.dx
 
