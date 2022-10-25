0c1f2ebd961b0e9bc91e683746e30130328b4a59
mattragoza <mtr22@pitt.edu>
2022-10-24 20:28:31 -0400

    update PINO model architecture, don't use FFT

--- mre_pinn/model/pino.py
+++ mre_pinn/model/pino.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch import nn
 
@@ -20,11 +21,12 @@ class PINO(torch.nn.ModuleList):
     ):
         super().__init__()
 
-        self.fc_input = torch.nn.Linear(n_spatial_dims + n_channels_in, n_hidden)
+        self.fc_in = torch.nn.Linear(n_channels_in, n_hidden)
 
         self.blocks = []
         for i in range(n_blocks):
             block = PINOBlock(
+                n_spatial_dims=n_spatial_dims,
                 n_channels_in=n_hidden,
                 n_channels_out=n_hidden,
                 n_modes=n_modes,
@@ -33,83 +35,104 @@ class PINO(torch.nn.ModuleList):
             self.blocks.append(block)
             self.add_module(f'block{i}', block)
 
-        self.fc_output = torch.nn.Linear(n_hidden, n_output)
+        self.conv_out = SpectralConv3d(
+            n_spatial_dims, n_hidden, n_channels_out, n_modes
+        )
+        self.regularizer = None
 
-    def forward(self, a, x):
+    def forward(self, inputs):
         '''
         Args:
-            a: (batch_size, n_x, n_y, n_z, n_channels_in) tensor
-            x: (batch_size, n_x, n_y, n_z, n_spatial_dims) tensor
+            inputs: Tuple of input tensors:
+                a: (batch_size, n_x, n_y, n_z, n_channels_in) tensor
+                x: (batch_size, n_x, n_y, n_z, n_spatial_dims) tensor
+                y: (batch_size, n_x, n_y, n_z, n_spatial_dims) tensor
         Returns:
-            u: (batch_size, n_x, n_y, n_z, n_channels_out) tensor
+            u: (batch_size, n_x, n_y, n_z, n_channels_out) output tensor
         '''
-        h = self.fc_input(torch.cat([a, x], dim=-1))
+        a, x, y = inputs
+        h = self.fc_in(a)
         for block in self.blocks:
-            h = block(h)
-        u = self.fc_output(h)
+            h = block(h, x)
+        u = self.conv_out(h, x, y)
         return u
 
 
 class PINOBlock(torch.nn.Module):
 
-    def __init__(self, n_channels_in, n_channels_out, n_modes, activ_fn):
+    def __init__(
+        self, n_spatial_dims, n_channels_in, n_channels_out, n_modes, activ_fn
+    ):
         super().__init__()
-        self.conv = SpectralConv3d(n_channels_in, n_channels_out, n_modes)
+        self.conv = SpectralConv3d(
+            n_spatial_dims, n_channels_in, n_channels_out, n_modes
+        )
         self.fc = torch.nn.Linear(n_channels_in, n_channels_out)
         self.activ_fn = get_activ_fn(activ_fn)
 
-    def forward(self, h):
+    def forward(self, h, x):
         '''
         Args:
-            h: (batch_size, n_channels_in, n_x, n_y, n_z)
+            h: (batch_size, n_x, n_y, n_z, n_channels_in)
+            x: (batch_size, n_x, n_y, n_z, n_spatial_dims)
+            y: (batch_size, n_x, n_y, n_z, n_spatial_dims)
         Returns:
-            out: (batch_size, n_channels_out, n_x, n_y, n_z)
+            out: (batch_size, n_x, n_y, n_z, n_channels_out)
         '''
-        return self.activ_fn(self.conv(h) + self.fc(h))
+        h = self.conv(h, x, x) + self.fc(x)
+        return self.activ_fn(h)
 
 
 class SpectralConv3d(torch.nn.Module):
 
-    def __init__(self, n_channels_in, n_channels_out, n_modes=16):
+    def __init__(self, n_spatial_dims, n_channels_in, n_channels_out, n_modes=16):
         super().__init__()
+        self.n_spatial_dims = n_spatial_dims
         self.n_channels_in = n_channels_in
         self.n_channels_out = n_channels_out
         self.n_modes = n_modes
 
+        scale = 1e-3
+        self.modes = nn.Parameter(scale * torch.rand(
+            (n_modes, n_spatial_dims), dtype=torch.float32
+        ))
         scale = 1 / (n_channels_in * n_channels_out)
-        self.weights = nn.Parameter(scale * torch.rand(
-            n_modes, n_modes, n_modes, n_channels_in, n_channels_out,
-            dtype=torch.complex64
+        self.kernel = nn.Parameter(scale * torch.rand(
+            (n_modes, n_channels_in, n_channels_out), dtype=torch.complex64
         ))
 
-    def forward(self, h):
+    def __repr__(self):
+        return f'SpectralConv3d(n_spatial_dims={self.n_spatial_dims}, n_channels_in={self.n_channels_in}, n_channels_out={self.n_channels_out}, n_modes={self.n_modes})'
+
+    def forward(self, h, x, y):
         '''
         Args:
             h: (batch_size, n_x, n_y, n_z, n_channels_in)
+            x: (batch_size, n_x, n_y, n_z, n_spatial_dims)
+            y: (batch_size, n_x, n_y, n_z, n_spatial_dims)
         Returns:
-            out: (batch_size, n_x, n_y, n_z, n_channels_out)
+            g: (batch_size, n_x, n_y, n_z, n_channels_in)
         '''
         assert h.shape[-1] == self.n_channels_in
+        assert x.shape[-1] == self.n_spatial_dims
+        assert y.shape[-1] == self.n_spatial_dims
+
         batch_size = h.shape[0]
         spatial_dims = (1, 2, 3)
         n_x, n_y, n_z = h.shape[1:4]
 
-        F_h = torch.fft.rfftn(h, dim=spatial_dims)
-        n = self.n_modes
-        F_out = torch.zeros(batch_size, n_x, n_y, n_z, self.n_channels_out)
-        F_out[:,:n,:n,:n,:] = torch.einsum(
-            'bxyzi,xyzio->bxyzo', F_h[:,:n,:n,:n,:], self.weights
-        )
-        return torch.fft.irfftn(F_out, s=(n_x, n_y, n_z), dim=spatial_dims)
+        # convert to frequency domain
+        x_dot_s = torch.einsum('bxyzd,fd->bxyzf', x, self.modes)
+        phi_x = torch.exp(-2j * np.pi * x_dot_s)
+        H = torch.einsum('bxyzi,bxyzf->bfi', h + 0j, phi_x)
 
+        # frequency-domain convolution
+        G = torch.einsum('bfi,fio->bfo', H, self.kernel)
 
-class FFT(torch.autograd.Function):
-    
-    @staticmethod
-    def forward(ctx, a, x):
-        ctx.save_for_backward(a, x)
-        return torch.fft.fftn(a)
+        # convert back to spatial domain
+        y_dot_s = torch.einsum('bxyzd,fd->bxyzf', y, self.modes)
+        phi_y = torch.exp(2j * np.pi * y_dot_s)
+        g = torch.einsum('bfo,bxyzf->bxyzo', G, phi_y).real
+        print(g.shape)
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, = ctx.saved_tensors
+        return g
--- mre_pinn/training/pino_training.py
+++ mre_pinn/training/pino_training.py
@@ -14,36 +14,71 @@ class PINOData(deepxde.data.Data):
         self.batch_size = batch_size
 
     def losses(self, targets, outputs, loss_fn, inputs, model, aux=None):
-        return [data_loss, pde_loss]
+        (a, x, y), (u_true,) = inputs, targets
+        u_pred = outputs
+        data_loss = loss_fn(u_true, u_pred)
+        print(data_loss.shape)
+        #pde_res = self.pde(x, u_pred, mu_pred)
+        pde_loss = 0 #loss_fn(0, pde_res)
+        return [data_loss, data_loss]
+
+    def get_tensors(self, idx):
+        '''
+        Args:
+            idx: Patient index in cohort.
+        Returns:
+            input: Tuple of input tensors.
+            target: Tuple of target tensors.
+        '''
+        patient = self.cohort[idx]
+
+        a_sequences = [
+            't1_pre_in', 't1_pre_out', 't1_pre_water', 't1_pre_fat'
+        ]
+        a_arrays = [patient.arrays[seq].values for seq in a_sequences]
+        a = np.stack(a_arrays, axis=-1)
+        a = torch.tensor(a, device='cuda', dtype=torch.float32)
+
+        x = patient.arrays['t1_pre_in'].field.points(reshape=False)
+        x = torch.tensor(x, device='cuda', dtype=torch.float32)
+
+        u = patient.arrays['wave'].values[...,None]
+        u = torch.tensor(u, device='cuda', dtype=torch.float32)
+
+        y = patient.arrays['wave'].field.points(reshape=False)
+        y = torch.tensor(y, device='cuda', dtype=torch.float32)
+        
+        mu = patient.arrays['mre'].values[...,None]
+        mu = torch.tensor(mu, device='cuda', dtype=torch.float32)
+
+        z = patient.arrays['mre'].field.points(reshape=False)
+        z = torch.tensor(z, device='cuda', dtype=torch.float32)
+
+        return (a, x, y), u
 
     def train_next_batch(self, batch_size=None):
         '''
         Args:
-            batch_size
+            batch_size: Number of patients in batch.
         Returns:
-            inputs: (batch_size, patch_size, n_input) input arrays
-            targets: (batch_size, patch_size, n_output) target arrays
+            inputs: Tuple of input tensors.
+            targets: Tuple of target tensors.
         '''
         batch_size = batch_size or self.batch_size
         batch_inds = self.batch_sampler.get_next(batch_size)
 
+        inputs, targets = [], []
         for idx in batch_inds:
-            patient = self.cohort[idx]
-            anat_sequences = ['t1_pre_water', 't1_pre_out', 't1_pre_fat', 't2']
-            a = np.stack([patient.arrays[seq].values for seq in anat_sequences])
-            a = torch.tensor(a, device='cuda', dtype=torch.float32)
-            u = patient.arrays['wave']
-            u = torch.tensor(u, device='cuda', dtype=torch.float32)
-            mu = patient.arrays['mre']
-            mu = torch.tensor(mu, device='cuda', dtype=torch.float32)
+            input, target = self.get_tensors(idx)
+            inputs.append(input)
+            targets.append(target)
 
-            x = patient.arrays['mask'].field.points()
-            mask = patient.arrays['mask'].field.values()
-
+        inputs = tuple(torch.stack(x) for x in zip(*inputs))
+        targets = torch.stack(targets)
         return inputs, targets
 
     def test(self):
-        return inputs, targets
+        return self.train_next_batch()
 
 
 class PINOModel(deepxde.Model):
@@ -54,7 +89,7 @@ class PINOModel(deepxde.Model):
         data = PINOData(cohort, pde, batch_size)
 
         # initialize the network weights
-        net.init_weights()
+        #TODO net.init_weights()
 
         super().__init__(data, net)
 
